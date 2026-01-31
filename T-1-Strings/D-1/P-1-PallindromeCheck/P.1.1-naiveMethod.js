afd5f7f3d4da7b3d86cf820e50bc2090f25f5378
RishabhVerma-SDE <rishabh.verma.sde@gmail.com>
2026-02-01 01:06:38 +0530

    Naive method for Pallindrome string

---
+++ T-1-Strings/D-1/P-1-PallindromeCheck/P.1.1-naiveMethod.js
@@ -0,0 +1,17 @@
+
+function isPallindrome(src) {
+    // there is no reverse function that is directly call here!
+    // we can also check for lower case elementer as Level and level are also pallindrome if we ignore the case
+    const reversedSrc = src.split('').reduce((acc,item)=> acc = item + acc, ''); // master stroke
+    const stringSize = src.length;
+    for(let itr = 0; itr < stringSize; ++itr ){
+        if (reversedSrc[itr] !== src[itr]){
+            return false;
+        }
+    }
+    return true;
+}
+
+console.log(isPallindrome('ABBA'));
+console.log(isPallindrome('ABCBA'));
+console.log(isPallindrome('TAMATAM'));
---
+++ T-1-Strings/D-1/P-1-PallindromeCheck/readme.md
@@ -0,0 +1,21 @@
+## Pallindrome String
+
+A string can be called a palindrome if we read it from forward or backward, it appears the same or in other words, we can say if we reverse a string and it is identical to the original string.
+
+# Example
+
+1. Str = ABBCBBA
+   O/P = True
+
+2. Str = ABCBA
+   O/P = True
+
+3. Str = TAMATAM
+   O?P = False
+
+
+# Ways to Solve (time/space)
+
+1. Naive Method theta(n)/theta(n)
+2. Efficient Method O(n)/O(1)
+3. Recursion Method
