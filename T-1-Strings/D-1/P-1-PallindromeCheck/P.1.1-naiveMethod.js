d1129b7c34e3d44d40bb7035dc7b5863bd26c535
RishabhVerma-SDE <rishabh.verma.sde@gmail.com>
2026-02-01 01:13:08 +0530

    Naive method for Pallindrome string

--- T-1-Strings/D-1/P-1-PallindromeCheck/P.1.1-naiveMethod.js
+++ T-1-Strings/D-1/P-1-PallindromeCheck/P.1.1-naiveMethod.js
@@ -2,7 +2,7 @@
 function isPallindrome(src) {
     // there is no reverse function that is directly call here!
     // we can also check for lower case elementer as Level and level are also pallindrome if we ignore the case
-    const reversedSrc = src.split('').reduce((acc,item)=> acc = item + acc, ''); // master stroke
+    const reversedSrc = src.split('').reduce((acc,item)=> acc = item + acc, ''); // master stroke => take the item at end and new item at starting to get the reverse
     const stringSize = src.length;
     for(let itr = 0; itr < stringSize; ++itr ){
         if (reversedSrc[itr] !== src[itr]){
